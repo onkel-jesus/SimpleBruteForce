b0702d0b8fae8c990d958948b8c14dfcd3a6ea65
jesus <onkeljezuz@proton.me>
2024-08-31 15:45:58 +0200

    Add runtime timer

--- crack.py
+++ crack.py
@@ -1,5 +1,10 @@
 from string import ascii_letters, digits, punctuation
 from itertools import product
+import time
+
+start_time = time.time()
 
 for passcode in product(ascii_letters + digits + punctuation, repeat=4):
     print(passcode)
+
+print("[ %s seconds ]" % (time.time() - start_time))
