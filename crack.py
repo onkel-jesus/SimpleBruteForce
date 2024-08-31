ee232332d4bbaaa566b2a4bd946dbef1a7768158
jesus <onkeljezuz@proton.me>
2024-08-31 15:41:40 +0200

    Add punctuation

--- crack.py
+++ crack.py
@@ -1,5 +1,5 @@
-from string import ascii_letters, digits
+from string import ascii_letters, digits, punctuation
 from itertools import product
 
-for passcode in product(ascii_letters + digits, repeat=4):
+for passcode in product(ascii_letters + digits + punctuation, repeat=4):
     print(passcode)
